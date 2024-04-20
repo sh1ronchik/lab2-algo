5fefe78f050ce7539788a64674086c34abea1c3d
ArtemOvsyannikov <artemrotfront@gmail.com>
2024-04-20 22:12:10 +0300

    optimize csv.py

--- algorithms/dataIO/csv.py
+++ algorithms/dataIO/csv.py
@@ -1,13 +1,8 @@
 import csv
 
 class CSVWriter:
-    def __init__(self):
-        pass
-
     def write_to_csv(self, data):
         with open('data.csv', 'w', newline='') as csvfile:
-            fieldnames = ['rectangles', 'time_prep', 'time_answer', 'time_total', 'alg']
-            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer = csv.DictWriter(csvfile, fieldnames=['rectangles', 'time_prep', 'time_answer', 'time_total', 'alg'])
             writer.writeheader()
-            for row in data:
-                writer.writerow(row)
+            writer.writerows(data)
