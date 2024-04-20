014d3febc177dccfbe37d23985ebf8e0829a8bd2
ArtemOvsyannikov <artemrotfront@gmail.com>
2024-04-20 22:13:33 +0300

    optimize data_gen.py

--- algorithms/dataIO/data_gen.py
+++ algorithms/dataIO/data_gen.py
@@ -1,13 +1,8 @@
 class DataGenerator:
-    def __init__(self):
-        pass
-
     def create_rectangles(self, number):
         with open("data/rectangles.txt", "w") as rec_file:
-            for i in range(number):
-                rec_file.write(f"{10 * i} {10 * i} {10 * (2 * number - i)} {10 * (2 * number - i)}\n")
+            rec_file.writelines(f"{10 * i} {10 * i} {10 * (2 * number - i)} {10 * (2 * number - i)}\n" for i in range(number))
 
     def create_points(self, number):
         with open("data/points.txt", "w") as points_file:
-            for i in range(number):
-                points_file.write(f"{pow(1009 * i, 31) % (20 * number)} {pow(1013 * i, 31) % (20 * number)}\n")
+            points_file.writelines(f"{pow(1009 * i, 31) % (20 * number)} {pow(1013 * i, 31) % (20 * number)}\n" for i in range(number))
