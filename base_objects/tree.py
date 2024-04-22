eb40f42543f8c4599eb9c33d1019a7db471eecb3
ArtemOvsyannikov <artemrotfront@gmail.com>
2024-04-22 11:41:45 +0300

    modified tree.py

--- base_objects/tree.py
+++ base_objects/tree.py
@@ -5,17 +5,15 @@ class Tree:
         self.root = None
 
     def create_tree(self, left, right):
-        node = Node(left, right)
-        if left + 1 < right:
-            mid = (right + left) // 2
-            node.left_child = self.create_tree(left, mid)
-            node.right_child = self.create_tree(mid, right)
-        return node
+        if left + 1 >= right:
+            return Node(left, right)
+        mid = (right + left) // 2
+        return Node(left, right, self.create_tree(left, mid), self.create_tree(mid, right))
 
     def change_tree(self, node, left, right, value):
-        if left <= node.left_border and node.right_border <= right:
+        if left <= node.left_border <= right:
             return node.copy_of_node(value)
-        if right <= node.left_border or node.right_border <= left:
+        if node.left_border > right or node.right_border < left:
             return node
         new_node = node.copy_without_children()
         new_node.left_child = self.change_tree(node.left_child, left, right, value)
@@ -26,20 +24,16 @@ class Tree:
         if not node.right_child and not node.left_child:
             return node.value
         mid = (node.right_border + node.left_border) // 2
-        if target < mid:
-            return node.value + self.find(node.left_child, target)
-        return node.value + self.find(node.right_child, target)
+        return node.value + (self.find(node.left_child, target) if target < mid else self.find(node.right_child, target))
 
     def bin_search(self, mass, target):
         if target < mass[0] or target > mass[-1]:
             return -1
-        left, right = 0, len(mass)
-        while right - left > 1:
-            mid = (right + left) // 2
-            if mass[mid] >= target:
-                right = mid
+        left, right = 0, len(mass) - 1
+        while left < right:
+            mid = (left + right) // 2
+            if mass[mid] < target:
+                left = mid + 1
             else:
-                left = mid
-        if mass[right] == target:
-            return right
-        return left
+                right = mid
+        return left if mass[left] == target else -1
