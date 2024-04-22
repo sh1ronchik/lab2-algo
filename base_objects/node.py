1111baa952dcbc64a3a6e4201246dfb3740fc390
ArtemOvsyannikov <artemrotfront@gmail.com>
2024-04-22 11:11:30 +0300

    optimize node,py

--- base_objects/node.py
+++ base_objects/node.py
@@ -1,17 +1,11 @@
 class Node:
-    def __init__(self, left=None, right=None):
+    def __init__(self, left=None, right=None, value=0):
         self.left_border, self.right_border = left, right
         self.left_child, self.right_child = None, None
-        self.value = 0
+        self.value = value
 
     def copy_without_children(self):
-        new_node = self.__class__(self.left_border, self.right_border)
-        new_node.value = self.value
-        return new_node
+        return self.__class__(self.left_border, self.right_border, self.value)
 
     def copy_of_node(self, v):
-        new_node = self.__class__(self.left_border, self.right_border)
-        new_node.value = self.value + v
-        new_node.left_child = self.left_child
-        new_node.right_child = self.right_child
-        return new_node
+        return self.__class__(self.left_border, self.right_border, self.value + v, self.left_child, self.right_child)
